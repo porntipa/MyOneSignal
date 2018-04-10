9c3a4f19a82ad0a8f9ec8546d50ec4d4a0fb855d
Porntipa Choksungnoen <porntipa.chok@gmail.com>
2018-04-10 11:10:50 +0700

    display PlayerID & Token

--- app/src/main/java/th/or/nectec/myonesignal/YourAppClass.java
+++ app/src/main/java/th/or/nectec/myonesignal/YourAppClass.java
@@ -1,18 +1,67 @@
 package th.or.nectec.myonesignal;
 
 import android.app.Application;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 
+import com.onesignal.OSNotification;
+import com.onesignal.OSNotificationOpenResult;
 import com.onesignal.OneSignal;
 
 public class YourAppClass extends Application {
    @Override
    public void onCreate() {
       super.onCreate();
-     
-      // OneSignal Initialization
+
+      // Initial OneSignal Push Notification
       OneSignal.startInit(this)
-        .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
-        .unsubscribeWhenNotificationsAreDisabled(true)
-        .init();
+              .setNotificationReceivedHandler(new OneSignal.NotificationReceivedHandler() {
+                 @Override
+                 public void notificationReceived(OSNotification notification) {
+                    //notification.payload.additionalData
+                    Log.d("debug", notification.payload.rawPayload); // notification.payload.additionalData.toString();
+
+                    Intent receivedNewMsgComplete = new Intent(CMConstant.RECEIVED_NEW_MESSAGE);//เตรียมส่งจ.ม.
+                    receivedNewMsgComplete.putExtra(CMConstant.RECEIVED_MESSAGE, notification.payload.body);// payload แนบข้อความ
+                    LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(receivedNewMsgComplete);//ส่ง
+                 }
+              })
+              .setNotificationOpenedHandler(new OneSignal.NotificationOpenedHandler() {
+                 @Override
+                 public void notificationOpened(OSNotificationOpenResult result) {
+                    Log.d("debug", "NotificationOpened");
+                 }
+              })
+              .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.None)
+              .init();
+
+      OneSignal.idsAvailable(new OneSignal.IdsAvailableHandler() {
+         @Override
+         public void idsAvailable(final String playerID, final String token) {
+
+            new Handler().postDelayed(new Runnable() {
+               @Override
+               public void run() {
+                  if (playerID != null) {
+                     Log.i(CMConstant.RECEIVED_PLAYERID, playerID);
+
+
+                     // notifiy token and playerid via broadcast
+                     Intent registrationComplete = new Intent(CMConstant.REGISTRATION_COMPLETE);
+                     registrationComplete.putExtra(CMConstant.RECEIVED_TOKEN, token);
+                     registrationComplete.putExtra(CMConstant.RECEIVED_PLAYERID, playerID);
+                     LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(registrationComplete);
+                  }
+               }
+            }, 1000);
+
+
+
+         }
+      });
+
+
    }
 }
